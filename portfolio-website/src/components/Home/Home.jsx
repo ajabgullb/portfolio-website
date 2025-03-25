364e6d0f124e2243f00bb66c80af8aa4068b7948
Ajab Gull Bhatti <ajabgullb.superior@gmail.com>
2025-03-25 14:32:51 +0500

    Handled an API and show the Quotations on the Home Page

--- portfolio-website/src/components/Home/Home.jsx
+++ portfolio-website/src/components/Home/Home.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from "axios"
 
-function Home() {
+export default function Home() {
+  const [quotation, setQuatation] = useState(null)
+  const apiUrl = "https://api.freeapi.app/api/v1/public/quotes/quote/random"
+
+  const randomQuotation = async () => {
+    try {
+      const res = await axios.get(apiUrl)
+      if (res) {
+        return res.data
+      }
+    } catch (error) {
+      console.error("Error", error)
+      return {success: false, error: error}
+    }
+  }
+
+  useEffect(() => {
+    const fetchQuotation = async () => {
+      const data = await randomQuotation()
+      if (data) setQuatation(data);
+    }
+
+    fetchQuotation()
+  }, [apiUrl])
+
   return (
     <div>
-      Home
+      {quotation && (
+          <div className='my-5'>
+            <span className='font-bold'>
+            {quotation.data.author}
+            </span>
+            {": "}
+            {quotation.data.content}
+          </div>
+      )}
     </div>
   )
 }
 
-export default Home
