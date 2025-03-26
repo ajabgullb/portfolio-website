46053180227e9d807d83a658de5562f7de921a5d
Ajab Gull Bhatti <ajabgullb.superior@gmail.com>
2025-03-26 13:15:40 +0500

    Intstalled Appwrite instead of Supabase

--- portfolio-website/src/components/Home/Home.jsx
+++ portfolio-website/src/components/Home/Home.jsx
@@ -1,43 +1,9 @@
-import React, { useState, useEffect } from 'react'
-import axios from "axios"
+import React from 'react'
 
 export default function Home() {
-  const [quotation, setQuatation] = useState(null)
-  const apiUrl = "https://api.freeapi.app/api/v1/public/quotes/quote/random"
-
-  const randomQuotation = async () => {
-    try {
-      const res = await axios.get(apiUrl)
-      if (res) {
-        return res.data
-      }
-    } catch (error) {
-      console.error("Error", error)
-      return {success: false, error: error}
-    }
-  }
-
-  useEffect(() => {
-    const fetchQuotation = async () => {
-      const data = await randomQuotation()
-      if (data) setQuatation(data);
-    }
-
-    fetchQuotation()
-  }, [apiUrl])
 
   return (
-    <div>
-      {quotation && (
-          <div className='my-5'>
-            <span className='font-bold'>
-            {quotation.data.author}
-            </span>
-            {": "}
-            {quotation.data.content}
-          </div>
-      )}
-    </div>
+    <div>Home</div>
   )
 }
 
