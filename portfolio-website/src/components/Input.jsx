74f59c7429c58cf18a098f0fc89b01051bb236ff
Ajab Gull Bhatti <ajabgullb.superior@gmail.com>
2025-03-27 15:38:19 +0500

    Made the Post Component

--- portfolio-website/src/components/Input.jsx
+++ portfolio-website/src/components/Input.jsx
@@ -7,6 +7,7 @@ export default function Input({label, type, value, placeholder, ...props}, ref) 
       <input 
         type={type}
         value={value}
+        ref={ref}
         placeholder={placeholder}
         className={`w-full mt-2 mb-2 px-2 py-4 rounded-lg border`}
         {...props}
